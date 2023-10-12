9256dfe411decca066d59b9e3616f67b51265e79
Lubos Kolumber <lubosk158@gmail.com>
2023-10-12 23:26:41 +0200

    Refactoring - create lookup hoos

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -16,7 +16,7 @@ const useGenres = () => useQuery({
   queryKey: ['genres'],
   queryFn: apiClient.getAll,
   staleTime: 24*60*60*1000, //24 hodin
-  initialData: {count: genres.length, results: genres}
+  initialData: genres
 })
 
 export default useGenres;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,6 @@
-import platforms from "../data/platforms";
 import { useQuery } from "@tanstack/react-query";
-import APIClient, {FetchResponse}  from "../services/api-client";
+import platforms from "../data/platforms";
+import APIClient from "../services/api-client";
 
 
 export interface Platform {
@@ -16,7 +16,7 @@ const usePlatforms = () => useQuery({
   queryKey: ['platforms'],
   queryFn: apiClient.getAll,
   staleTime: 24*60*60*1000, //24 hodin
-  initialData: {count: platforms.length, results: platforms}
+  initialData: platforms
 })
 
 export default usePlatforms;
