6b18d6be9aea60a76b738f1946123df8ab79ff4c
Lubos Kolumber <lubosk158@gmail.com>
2023-10-12 14:21:23 +0200

    Implement infinite scrolling

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -30,7 +30,8 @@ const useGames = (gameQuery: GameQuery) => useInfiniteQuery<FetchResponse<Game>,
   keepPreviousData: true,
   getNextPageParam: (lastPage, allPages) =>{
     return lastPage.next ? allPages.length + 1: undefined;
-  }
+  },
+  staleTime: 24*60*60*1000
 });
 
 
