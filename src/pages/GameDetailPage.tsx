fd1f835ab6c4e3a6a81f02f5ffb805ab7bf64a54
Lubos Kolumber <lubosk158@gmail.com>
2023-10-17 18:18:11 +0200

    Build game attributes

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,7 +1,10 @@
-import { Button, Heading, Spinner, Text } from "@chakra-ui/react";
+import { Button, Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
+import CriticScore from "../components/CriticScore";
+import DefinitionItem from "../components/DefinitionItem";
 import ExpandableText from "../components/ExpandableText";
+import GameAttributes from "../components/GameAttributes";
 import useGame from "../hooks/useGame";
 
 const GameDetailPage = () => {
@@ -9,17 +12,16 @@ const GameDetailPage = () => {
 
   const { data: game, isLoading, error } = useGame(slug!);
 
-  // Step 1: Create a state variable
-  const [showFullText, setShowFullText] = useState(false);
-
   if (isLoading) return <Spinner />;
   if (error || !game) throw error;
+
+  console.log();
 
   return (
     <>
       <Heading>{game.name}</Heading>
-
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={game} />
     </>
   );
 };
